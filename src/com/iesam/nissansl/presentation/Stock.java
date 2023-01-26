be16b901f73eb8392a720a36dd7514618e895f62
Pablo Ajates <pablo.ajasan@educa.jcyl.es>
2023-01-26 12:17:20 +0100

    fichero unidades

---
+++ src/com/iesam/nissansl/presentation/Stock.java
@@ -0,0 +1,12 @@
+package com.iesam.nissansl.presentation;
+
+import com.iesam.nissansl.domain.models.Accesorios;
+
+public class Stock {
+
+    public void stock (Accesorios accesorios) {
+
+
+
+    }
+}
